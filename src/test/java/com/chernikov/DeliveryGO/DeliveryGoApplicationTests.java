e2fadb18830e4944206abab3a1abd7d64c48718d
Ilya Chernikov <dddryinside@gmail.com>
2024-09-21 12:03:03 +0300

    Initial commit

---
+++ src/test/java/com/chernikov/DeliveryGO/DeliveryGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chernikov.DeliveryGO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
