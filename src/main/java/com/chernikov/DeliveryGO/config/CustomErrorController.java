e3fadca4dffe575409764c71e8ae006aef1882fd
Ilya Chernikov <dddryinside@gmail.com>
2024-10-20 13:00:56 +0300

    Custom page for errors

---
+++ src/main/java/com/chernikov/DeliveryGO/config/CustomErrorController.java
@@ -0,0 +1,25 @@
+package com.chernikov.DeliveryGO.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute("jakarta.servlet.error.status_code");
+        String statusCode = status != null ? status.toString() : "Unknown";
+
+        Object message = request.getAttribute("jakarta.servlet.error.message");
+        String errorMessage = message != null ? message.toString() : "No details available";
+
+        model.addAttribute("error", "Error: " + statusCode);
+        model.addAttribute("message", errorMessage);
+
+        return "error";
+    }
+}
--- src/main/java/com/chernikov/DeliveryGO/config/GlobalExceptionHandler.java
+++ src/main/java/com/chernikov/DeliveryGO/config/GlobalExceptionHandler.java
@@ -4,13 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
-
-import javax.naming.AuthenticationException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -26,4 +22,5 @@ public class GlobalExceptionHandler {
         logger.error(e.getMessage());
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
 }
