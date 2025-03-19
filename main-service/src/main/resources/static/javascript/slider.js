b802e39af6573b3e7fe94edd06c90fbc0d93a4d3
Ilya Chernikov <dddryinside@gmail.com>
2025-03-19 15:50:31 +0300

    added slider

---
+++ main-service/src/main/resources/static/javascript/slider.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    function startSlider() {
+        const slider = document.getElementById('slider');
+        if (!slider) return;
+
+        let index = 0;
+        const slides = document.querySelectorAll('.slide');
+        const totalSlides = slides.length;
+
+        setInterval(() => {
+            index = (index + 1) % totalSlides;
+            slider.style.transform = `translateX(${-index * 800}px)`;
+        }, 3000);
+    }
+
+    setTimeout(startSlider, 1000);
+});
